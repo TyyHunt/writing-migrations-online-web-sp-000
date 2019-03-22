233a10554bc31b188e17c8dba7727f7f8d0fd8dc
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 20:06:46 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
 class AddGradeAndBirthday < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :artists, :favorite_food, :string
-    add_column :artists, :favorite_food, :string
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
   end
 
 end
